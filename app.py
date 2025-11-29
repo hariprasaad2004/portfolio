ba48f805554b0f5f57ec05b08d1abee66c4b2069
hariprasaad2004 <hariprasaadmurugesan2004@gmail.com>
2025-11-30 01:13:07 +0530

    added homepage route

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+from flask import Flask, request, jsonify, send_from_directory
+import os
 from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
+@app.route("/")
+def home():
+    return send_from_directory("static", "index.html")
 # ----------------------------------------------------
 # 🔶 FILL THIS SECTION WITH YOUR GMAIL & APP PASSWORD
 # ----------------------------------------------------
